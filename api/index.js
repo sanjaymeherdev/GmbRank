d9a0d40a2b38f0199d92ab0549c191de27c6aa09
Sanjay Meher <digitalpgmb@gmail.com>
2026-06-04 03:25:23 +0000

    cty

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 import authRoutes from '../routes/auth.js';
 import businessRoutes from '../routes/businesses.js';
@@ -8,6 +10,7 @@ import keywordSetRoutes, { updateKeywordSet, deleteKeywordSet } from '../routes/
 import rankingRoutes from '../routes/rankings.js';
 import { requireAuth } from '../middleware/auth.js';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 app.use(express.json());
@@ -21,5 +24,10 @@ app.use('/api/locations/:locationId/keyword-sets', keywordSetRoutes);
 app.put('/api/keyword-sets/:id', requireAuth, updateKeywordSet);
 app.delete('/api/keyword-sets/:id', requireAuth, deleteKeywordSet);
 app.use('/api/keyword-sets', rankingRoutes);
+
+app.use(express.static(join(__dirname, '../public')));
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, '../public/index.html'));
+});
 
 export default app;
